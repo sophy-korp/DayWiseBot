a519720accc9e08c05c6d9716049b2c89b7985c6
sophy-korp <108880258+sophy-korp@users.noreply.github.com>
2024-01-04 19:27:39 +0300

    Delete api.py

--- api.py
+++
@@ -1,7 +0,0 @@
-telegram_api = '6756741753:AAEEvn9EGOOvVa47GZDXN8OqUzuoA9cjcMY'
-openweather_api = '70ace1e09cefbe1713f28e1b8cae72da'
-mapquest_api = 'C4AbGDp5IugUBKmLeFricsHtJxKLeJeq'
-deepai_api = '4758184c-68e1-4d64-9ea8-829fac9bad16'
-exchange_api = '25ef9b0f8d2a6656bae8f403'
-news_api = '028a3fd8eb614a8f8c6906fad32d1748'
-historical_events_api = '8zIzRZgqADvPAVYZmiFlig==1z5oJ7tPR2wCZAAG'
